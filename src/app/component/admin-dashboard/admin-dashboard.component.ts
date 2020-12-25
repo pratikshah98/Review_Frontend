e3a33caabb30f2820da936b557f3431b34a91e85
Bunny899 <prarthit.shah.123@gmail.com>
2020-12-25 11:11:16 +0530

    dashboard boxes done

--- src/app/component/admin-dashboard/admin-dashboard.component.ts
+++ src/app/component/admin-dashboard/admin-dashboard.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from "@angular/material/table";
 import { MatSort } from "@angular/material/sort";
 import { animation } from '@angular/animations';
 import { Chart } from 'chart.js';
+import { AdminService } from "../../service/admin.service";
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
@@ -21,9 +22,25 @@ LineChart = [];
   PieChart=[];
   myPieChart=[];
 
-  constructor() { }
+  totalInternship:number;
+  totalCompany:number;
+  totalReview:number;
+  totalStudent:number;
+  constructor(private adminService:AdminService) { }
 
   ngOnInit(): void {
+    this.adminService.getCountByInternship().subscribe((data:any)=>{
+      this.totalInternship=data[0].internshipcnt;
+    });
+    this.adminService.getCountByCompany().subscribe((data:any)=>{
+      this.totalCompany=data[0].companycnt;
+    });
+    this.adminService.getCountByReview().subscribe((data:any)=>{
+      this.totalReview=data[0].reviewcnt;
+    });
+    this.adminService.getCountByStudent().subscribe((data:any)=>{
+      this.totalStudent=data[0].studentcnt;
+    });
   }
 
 }
--- src/app/studentcomponent/dashboard/dashboard.component.ts
+++ src/app/studentcomponent/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import { AdminService } from "../../service/admin.service";
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -10,9 +11,25 @@ import {MatTableDataSource} from '@angular/material/table';
 export class DashboardComponent implements OnInit {
   displayedColumns: string[] = ['Exam_name','Total_marks','obtained_marks','percent','date'];
   dataSource=new MatTableDataSource();
-  constructor() { }
-
+  totalInternship:number;
+  totalCompany:number;
+  totalReview:number;
+  totalStudent:number;
+  constructor(private adminService:AdminService) { }
+  
   ngOnInit(): void {
+    this.adminService.getCountByInternship().subscribe((data:any)=>{
+      this.totalInternship=data[0].internshipcnt;
+    });
+    this.adminService.getCountByCompany().subscribe((data:any)=>{
+      this.totalCompany=data[0].companycnt;
+    });
+    this.adminService.getCountByReview().subscribe((data:any)=>{
+      this.totalReview=data[0].reviewcnt;
+    });
+    this.adminService.getCountByStudent().subscribe((data:any)=>{
+      this.totalStudent=data[0].studentcnt;
+    });
   }
 
 }
